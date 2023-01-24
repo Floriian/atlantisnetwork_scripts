7c5ae084d98b1c301bff0f36d45b15d426eb86f2
Floriian <lovasifloriian@outlook.hu>
2023-01-24 23:54:59 +0100

    add docker run command back.

--- setup-containers.sh
+++ setup-containers.sh
@@ -20,7 +20,7 @@ then
   echo $username
   echo $password
   echo $baseDB
-
+  docker run --name $containerName -e MYSQL_ROOT_PASSWORD=$password MYSQL_USER=$username MYSQL_PASSWORD=$password MYSQL_DATABASE=$baseDB mysql:8.0 -d 
 elif [ $((serviceCode)) -eq 2 ]
 then
   echo PostgreSQL
